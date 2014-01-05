4693ffa0f652feb84c973145e13ef0bf9759d3e7
jt1134 <jt1134@gmail.com>
2014-01-04 20:52:27 -0600

    Camera2: implement volume key zoom in video mode
    Change-Id: I140e8cfb3e39700f60e40c35b38e92c83a3c26de

--- src/com/android/camera/VideoModule.java
+++ src/com/android/camera/VideoModule.java
@@ -1368,6 +1368,12 @@ public class VideoModule implements CameraModule,
         }
 
         switch (keyCode) {
+            case KeyEvent.KEYCODE_VOLUME_UP:
+                mUI.onScaleStepResize(true);
+                return true;
+            case KeyEvent.KEYCODE_VOLUME_DOWN:
+                mUI.onScaleStepResize(false);
+                return true;
             case KeyEvent.KEYCODE_CAMERA:
                 if (event.getRepeatCount() == 0) {
                     mUI.clickShutter();
@@ -1390,6 +1396,9 @@ public class VideoModule implements CameraModule,
     @Override
     public boolean onKeyUp(int keyCode, KeyEvent event) {
         switch (keyCode) {
+            case KeyEvent.KEYCODE_VOLUME_UP:
+            case KeyEvent.KEYCODE_VOLUME_DOWN:
+                return true;
             case KeyEvent.KEYCODE_CAMERA:
                 mUI.pressShutter(false);
                 return true;
