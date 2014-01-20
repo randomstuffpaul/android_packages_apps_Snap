66f40e55bd05d41b10c1fbeef391107dc4451063
Michael Bestas <mikeioannina@gmail.com>
2014-01-20 20:40:32 +0200

    Camera: Powerkey shutter (2/2)
    Ported from cm-10.1
    
    Including cm-10.1 camera commit:
    aa4ae80a41fbab763891c5ef6d67a9e5b4bb981b Don't mess around with pref local ID for power shutter initialization.
    
    Includes http://review.cyanogenmod.org/56986
    
    [mikeioannina]: Updates for L
    
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    Change-Id: I0992baa558eefd306d00fbece59cb5c512d9448b

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2012 The Android Open Source Project
+ * Copyright (C) 2013-2015 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -591,6 +592,9 @@ public class PhotoModule
         setupDualCameraMode();
 
         mUI = new PhotoUI(activity, this, parent);
+
+        // Power shutter
+        mActivity.initPowerShutter(mPreferences);
 
         if (mOpenCameraThread == null) {
             mOpenCameraThread = new OpenCameraThread();
@@ -2829,6 +2833,9 @@ public class PhotoModule
         // (e.g. onResume -> onPause -> onResume).
         stopPreview();
 
+        // Load the power shutter
+        mActivity.initPowerShutter(mPreferences);
+
         mNamedImages = null;
 
         if (mLocationManager != null) mLocationManager.recordLocation(false);
@@ -2978,6 +2985,9 @@ public class PhotoModule
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
+        /*TODO: if (!mActivity.mShowCameraAppView) {
+            return false;
+        }*/
         switch (keyCode) {
             case KeyEvent.KEYCODE_VOLUME_UP:
                 if (/*TODO: mActivity.isInCameraApp() &&*/ mFirstTimeInitialized
@@ -3015,12 +3025,21 @@ public class PhotoModule
                     mUI.pressShutterButton();
                 }
                 return true;
+            case KeyEvent.KEYCODE_POWER:
+                if (mFirstTimeInitialized && event.getRepeatCount() == 0
+                        && CameraActivity.mPowerShutter) {
+                    onShutterButtonFocus(true);
+                }
+                return true;
         }
         return false;
     }
 
     @Override
     public boolean onKeyUp(int keyCode, KeyEvent event) {
+        /*TODO: if (!mActivity.mShowCameraAppView) {
+            return false;
+        }*/
         switch (keyCode) {
             case KeyEvent.KEYCODE_VOLUME_UP:
             case KeyEvent.KEYCODE_VOLUME_DOWN:
@@ -3028,6 +3047,11 @@ public class PhotoModule
             case KeyEvent.KEYCODE_FOCUS:
                 if (mFirstTimeInitialized) {
                     onShutterButtonFocus(false);
+                }
+                return true;
+            case KeyEvent.KEYCODE_POWER:
+                if (CameraActivity.mPowerShutter && mFirstTimeInitialized) {
+                    onShutterButtonClick();
                 }
                 return true;
         }
@@ -4899,6 +4923,7 @@ public class PhotoModule
             setCameraParametersWhenIdle(UPDATE_PARAM_PREFERENCE);
             mUI.updateOnScreenIndicators(mParameters, mPreferenceGroup,
                 mPreferences);
+            mActivity.initPowerShutter(mPreferences);
         } else {
             mHandler.sendEmptyMessage(SET_PHOTO_UI_PARAMS);
         }
