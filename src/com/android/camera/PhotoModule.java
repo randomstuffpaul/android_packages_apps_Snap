71076e3fd669c72a424b59bba518fdd008d0a78c
Michael Bestas <mikeioannina@gmail.com>
2014-10-14 06:21:58 +0300

    Camera2: Add option to set max screen brightness
    * Set maximum screen brightness while the user is inside the camera app
    
    Change-Id: I8b16ba47a933bc7d6b0c1cd62bfd6ca54875ce1e

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -596,6 +596,9 @@ public class PhotoModule
         // Power shutter
         mActivity.initPowerShutter(mPreferences);
 
+        // Max brightness
+        mActivity.initMaxBrightness(mPreferences);
+
         if (mOpenCameraThread == null) {
             mOpenCameraThread = new OpenCameraThread();
             mOpenCameraThread.start();
@@ -2836,6 +2839,9 @@ public class PhotoModule
         // Load the power shutter
         mActivity.initPowerShutter(mPreferences);
 
+        // Load max brightness
+        mActivity.initMaxBrightness(mPreferences);
+
         mNamedImages = null;
 
         if (mLocationManager != null) mLocationManager.recordLocation(false);
@@ -4939,6 +4945,7 @@ public class PhotoModule
             mUI.updateOnScreenIndicators(mParameters, mPreferenceGroup,
                 mPreferences);
             mActivity.initPowerShutter(mPreferences);
+            mActivity.initMaxBrightness(mPreferences);
         } else {
             mHandler.sendEmptyMessage(SET_PHOTO_UI_PARAMS);
         }
