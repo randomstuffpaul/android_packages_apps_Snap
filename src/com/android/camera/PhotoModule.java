ce9f9b8702d8cf5f9544e1b2b4e75148b59e5c7c
Michael Bestas <mikeioannina@gmail.com>
2015-08-29 13:15:02 +0300

    Revert "SnapdragonCamera: Forbid volume key can take picture"
    This reverts commit e73e717b8be1ba834161914af4debc61c1feb8be.
    
    Change-Id: Iee719f34f6016f89ec0bb1c078bfa25c2f718436

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -2980,9 +2980,6 @@ public class PhotoModule
         switch (keyCode) {
             case KeyEvent.KEYCODE_VOLUME_UP:
             case KeyEvent.KEYCODE_VOLUME_DOWN:
-                if (CameraUtil.volumeKeyShutterDisable(mActivity)) {
-                   return false;
-                }
             case KeyEvent.KEYCODE_FOCUS:
                 if (/*TODO: mActivity.isInCameraApp() &&*/ mFirstTimeInitialized) {
                     if (event.getRepeatCount() == 0) {
@@ -3058,8 +3055,7 @@ public class PhotoModule
         switch (keyCode) {
             case KeyEvent.KEYCODE_VOLUME_UP:
             case KeyEvent.KEYCODE_VOLUME_DOWN:
-                if (/*mActivity.isInCameraApp() && */ mFirstTimeInitialized
-                        && !CameraUtil.volumeKeyShutterDisable(mActivity)) {
+                if (/*mActivity.isInCameraApp() && */ mFirstTimeInitialized) {
                     onShutterButtonClick();
                     return true;
                 }
