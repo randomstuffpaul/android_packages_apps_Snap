8fbde5e621739a94c5190c05f31fb471faa3d13f
Rashed Abdel-Tawab <rashed@linux.com>
2017-03-04 13:08:38 -0500

    Revert "SnapdragonCamera: Camera switches from bayer and mono on switching to front camera"
    Some devices (ex. LG G5, V20, and G6) have perfectly functional dual rear camera's that
    work fine. No reason to force the number of camera's to 2
    
    This reverts commit fab321f61b6fe4e1296806ff5b04bac19d1e4de6.
    
    Change-Id: I1ffb0defa299e107b7f193b7b72af3567c4947df

--- src/com/android/camera/CameraSettings.java
+++ src/com/android/camera/CameraSettings.java
@@ -1028,10 +1028,6 @@ public class CameraSettings {
             return;
         }
 
-        if (numOfCameras > 2 ) {
-            numOfCameras = 2;
-        }
-
         CharSequence[] entryValues = new CharSequence[numOfCameras];
         for (int i = 0; i < numOfCameras; ++i) {
             entryValues[i] = "" + i;
