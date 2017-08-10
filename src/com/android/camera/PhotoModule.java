d3dcb0fd5fa3c56bd9118b73f51d6d8dc5b1f831
Arne Coucheron <arco68@gmail.com>
2017-08-10 06:48:28 +0200

    Snap: Bring back original shutter icons and animation
    Change-Id: I843ba306053ea7f14fbf805c8633c02a854f84df

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -1135,6 +1135,8 @@ public class PhotoModule
                     return;
                 }
 
+                mUI.doShutterAnimation();
+
                 Location loc = getLocationAccordPictureFormat(mParameters.get(KEY_PICTURE_FORMAT));
 
                 mLongShotCaptureCount++;
--- src/com/android/camera/ui/CameraControls.java
+++ src/com/android/camera/ui/CameraControls.java
@@ -981,7 +981,7 @@ public class CameraControls extends RotatableLayout {
         View[] views = {
             mSceneModeSwitcher, mFilterModeSwitcher, mFrontBackSwitcher,
             TsMakeupManager.HAS_TS_MAKEUP ? mTsMakeupSwitcher : mHdrSwitcher,
-            mMenu, mPreview, mSwitcher, mMute, mReviewRetakeButton,
+            mMenu, mShutter, mPreview, mSwitcher, mMute, mReviewRetakeButton,
             mReviewCancelButton, mReviewDoneButton
         };
         for (View v : views) {
